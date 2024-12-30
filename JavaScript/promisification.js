5e27808d57e72564538d660336619ed776900f49
Sanjay-Tiwari <Sanjaytiwari@nimapinfotech.com>
2024-12-30 16:39:48 +0530

    promisification added

---
+++ JavaScript/promisification.js
@@ -0,0 +1,31 @@
+
+function functionToPromisify(num1 , num2 , callBack ) {
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        callBack(new Error("Both the Parameters should be valid numbers !")) ;
+    }else
+        setTimeout(() => {
+            callBack(null , num1 + num2) ;
+        }, 2000);
+}
+
+function promisify(func) {
+    return function(...args) {
+        return new Promise((resolve , reject) => {
+            func(...args , (err , res) => {
+                if(err) {
+                    console.log("promise rejected") ;
+                    reject(err) ;
+                }else{
+                    console.log("promise resolved") ;
+                    resolve(res) ;
+                }
+            }) ;
+        })
+    }
+}
+
+let promisified = promisify(functionToPromisify) ;
+
+promisified("a" , 7)
+    .then((res) => console.log(res))
+    .catch((err) => console.log("err : " , err)) ;
