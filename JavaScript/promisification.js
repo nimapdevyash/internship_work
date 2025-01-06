6e0986e199e5b26cbaf4a2ec94a80b59ce92b619
Sanjay-Tiwari <Sanjaytiwari@nimapinfotech.com>
2025-01-06 15:23:20 +0530

    formating changes

--- JavaScript/promisification.js
+++ JavaScript/promisification.js
@@ -1,31 +1,30 @@
-
-function functionToPromisify(num1 , num2 , callBack ) {
-    if(!Number.isInteger(num1) || !Number.isInteger(num2)) {
-        callBack(new Error("Both the Parameters should be valid numbers !")) ;
-    }else
-        setTimeout(() => {
-            callBack(null , num1 + num2) ;
-        }, 2000);
+function functionToPromisify(num1, num2, callBack) {
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+    callBack(new Error("Both the Parameters should be valid numbers !"));
+  } else
+    setTimeout(() => {
+      callBack(null, num1 + num2);
+    }, 2000);
 }
 
 function promisify(func) {
-    return function(...args) {
-        return new Promise((resolve , reject) => {
-            func(...args , (err , res) => {
-                if(err) {
-                    console.log("promise rejected") ;
-                    reject(err) ;
-                }else{
-                    console.log("promise resolved") ;
-                    resolve(res) ;
-                }
-            }) ;
-        })
-    }
+  return function (...args) {
+    return new Promise((resolve, reject) => {
+      func(...args, (err, res) => {
+        if (err) {
+          console.log("promise rejected");
+          reject(err);
+        } else {
+          console.log("promise resolved");
+          resolve(res);
+        }
+      });
+    });
+  };
 }
 
-let promisified = promisify(functionToPromisify) ;
+let promisified = promisify(functionToPromisify);
 
-promisified("a" , 7)
-    .then((res) => console.log(res))
-    .catch((err) => console.log("err : " , err)) ;
+promisified("a", 7)
+  .then((res) => console.log(res))
+  .catch((err) => console.log("err : ", err));
