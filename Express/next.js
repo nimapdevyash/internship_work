37c0a1964485d244188f1d794b93ba0a85118717
nimapdevyash <yash.sawarkar@nimapinfotech.com>
2025-01-31 12:49:20 +0530

    added an key note

--- Express/next.js
+++ Express/next.js
@@ -7,7 +7,9 @@ const app = express();
 function firstMiddleware(req, res, next) {
   console.log("first middleware called");
   next();
-  console.log("strill inside first middleware");
+  // can't modify the response at this point as the headers are already have been sent to the client , if uncommented below code it'll throw an error
+  // res.json("response from firstMiddleware");
+  console.log("inside first middleware after next call");
 }
 
 function secondMiddleware(req, res, next) {
